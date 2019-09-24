8cc0c799425df117868e429f7a62c325b0d5280b
Waldemar <wowa.lehner@gmail.com>
2019-09-24 20:55:02 +0200

    .

---
+++ Orcabot-Shared-Types__/Types/System.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Orcabot.Types.Enums;
+namespace Orcabot.Types
+{
+    public class System
+    {
+        public string Name { get; set; }
+        public Coordinate Coordinate { get; set; }
+        public List<Station> Stations { get; set; }
+        public Security SystemSecurity { get; set; }
+        public System()
+        {
+            Stations = new List<Station>();
+        }
+    }
+    public struct Coordinate
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Z { get; set; }
+    }
+}
