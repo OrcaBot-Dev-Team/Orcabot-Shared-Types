de1fb41ba5df2e73634167e899ab310666bb4745
Waldemar <wowa.lehner@gmail.com>
2019-09-26 20:07:18 +0200

    fixed #2 and #3

---
+++ Orcabot-Shared-Types/Helpers/JSONParser.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Orcabot.Types;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using Sys = Orcabot.Types.System;
+using System.IO;
+
+namespace Orcabot.Helpers
+{
+    static class JSONParser
+    {
+        /// <summary>
+        /// Returns a Dictionary of Systems if Parsing is successful. Returns null upon failure.
+        /// </summary>
+        /// <param name="path"> The URI of the File that contains the Systems JSON</param>
+        /// <returns>Null or a Dict<string,Sys></returns>
+        static public bool TryGetSystemsFromJSON(string path, out Dictionary<string, Sys> sys, out Exception e) {
+            sys = null;
+            e = null;
+            if (!File.Exists(path)) {
+                e = new FileNotFoundException(path);
+                return false;
+            }
+
+
+
+
+
+            try {
+                var dataAsString = File.ReadAllText(path);
+                sys = JsonConvert.DeserializeObject<Dictionary<string, Sys>>(dataAsString);
+            }
+            catch (Exception b) {
+                e = b;
+                return false;
+            }
+            return true;
+        }
+    }
+}
